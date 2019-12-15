77dd65639d3e04ca304b3e3aede9edfbb428ea6b
kaymyst <kaymyst@googlemail.com>
2019-12-15 18:41:12 +0000

    trying to package

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const electron = require("electron"),
 Tray= electron.Tray,
 Menu= electron.Menu,
-BrowserWindow = electron.BrowserWindow,
+MenuItem = electron.MenuItem,
+//BrowserWindow = electron.BrowserWindow,
   app = electron.app;
 const spawn = require("child_process").spawn;
 const _ = require("lodash");
+const path = require('path')
+const dirName = process.pkg && process.pkg.entrypoint ? path.dirname(process.execPath) : __dirname;
+const ffmpeg = require(app.getAppPath()+'/lib/findFfmpeg')(dirName);
 
-const node = spawn(
-  "node",
-  ["./ffmpeg-streamer"],
-  {
-    cwd: process.cwd()
-  }
-);
+//app.set('ffmpegVersion', ffmpeg.version)
+//app.set('ffmpegPath', ffmpeg.path)
+
+let arrayofdevices = [];
+const { exec } = require('child_process')
+var donevideo = false;
+
+
+
 function redirectOutput(x) {
   let lineBuffer = "";
 
@@ -29,29 +35,100 @@ function redirectOutput(x) {
     lineBuffer = lines[lines.length - 1];
   });
 }
+let node /*= spawn(
+  "node",
+  ["./ffmpeg-streamer"],
+  {
+    cwd: process.cwd()
+  }
+);*/
 
-
-      redirectOutput(node.stdout);
-      redirectOutput(node.stderr);
-      
 let tray;
 let mainWindow;
 
+function spawnFfmpeg(i){
+  if (node)
+    node.kill("SIGINT");
+
+  node = spawn(
+    "node",
+    [app.getAppPath()+"/ffmpeg-streamer",i, "production"],
+    {
+      cwd: process.cwd()
+    }
+  );
+
+  redirectOutput(node.stdout);
+  redirectOutput(node.stderr);
+}
+
+
+
 function createWindow() {
   // Create the browser window.
-  tray = new Tray('iconTemplate.png');
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio', checked: true  },
-    { label: 'Item2', type: 'radio' },
+  tray = new Tray(app.getAppPath()+'/iconTemplate.png');
+  let contextMenu = Menu.buildFromTemplate([
+
     { type: 'separator' },
     {label: 'Quit',
     click: () => {
+      if (node)
+        node.kill("SIGINT");
       app.quit();
     }
   }
   ])
+
+
+  var i=0;
+
+
+
   tray.setToolTip('This is my application.');
   tray.setContextMenu(contextMenu);
+
+  exec( 'ffmpeg -hide_banner -f avfoundation -list_devices true -i \"\"', (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      var lines = stderr.match(/^.*([\n\r]+|$)/gm);
+
+      lines.forEach(line => {
+        if (line.includes("AVFoundation audio devices:"))
+          donevideo = true;
+      if (line.includes( "[AVFoundation input device")&&!donevideo)
+      {
+        if (!line.includes("AVFoundation video devices:"))
+        {
+
+          var regExp = /(\[([^\]]|\[\])*\])/;
+          var matches = regExp.exec(line);
+          var device = line.replace(matches[1],"").trim();
+          arrayofdevices.push(device);
+          var index = i;
+
+          contextMenu.insert(i,new MenuItem(
+            {type:'radio',
+              label: device,
+            click: () => {
+              console.log(device);
+              spawnFfmpeg(index);
+              contextMenu.items[index].checked = true;
+              //console.log (contextMenu.items[index]);
+              tray.setContextMenu(contextMenu);
+
+            }
+          }
+          ));
+          i++;
+          console.log(`Video device : ${device}`)
+          tray.setContextMenu(contextMenu);
+          spawnFfmpeg(0);
+        }
+      }
+
+    });
+    }
+  })
 /*
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
@@ -70,7 +147,7 @@ function createWindow() {
     mainWindow = null;
   });*/
 }
-
+app.dock.hide();
 app.on("ready", createWindow);
 app.on("browser-window-created", function(e, window) {
 //  window.setMenu(null);
@@ -78,6 +155,8 @@ app.on("browser-window-created", function(e, window) {
 
 app.on("window-all-closed", function() {
   if (process.platform !== "darwin") {
+    if (node)
+      node.kill("SIGINT");
     app.quit();
   }
 });
