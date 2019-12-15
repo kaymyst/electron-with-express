6be6b486e4cc7d3053f650ffa96257465b215389
kaymyst <kaymyst@googlemail.com>
2019-12-15 08:29:26 +0000

    launch ffmpeg streamer

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
 const electron = require("electron"),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow;
+Tray= electron.Tray,
+Menu= electron.Menu,
+BrowserWindow = electron.BrowserWindow,
+  app = electron.app;
+const spawn = require("child_process").spawn;
+const _ = require("lodash");
 
+const node = spawn(
+  "node",
+  ["./ffmpeg-streamer"],
+  {
+    cwd: process.cwd()
+  }
+);
+function redirectOutput(x) {
+  let lineBuffer = "";
+
+  x.on("data", function(data) {
+    lineBuffer += data.toString();
+    let lines = lineBuffer.split("\n");
+
+    _.forEach(lines, l => {
+      if (l !== "") {
+        console.log(l);
+      }
+    });
+
+    lineBuffer = lines[lines.length - 1];
+  });
+}
+
+
+      redirectOutput(node.stdout);
+      redirectOutput(node.stderr);
+      
+let tray;
 let mainWindow;
 
 function createWindow() {
+  // Create the browser window.
+  tray = new Tray('iconTemplate.png');
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio', checked: true  },
+    { label: 'Item2', type: 'radio' },
+    { type: 'separator' },
+    {label: 'Quit',
+    click: () => {
+      app.quit();
+    }
+  }
+  ])
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
+/*
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 640,
@@ -20,12 +68,12 @@ function createWindow() {
   });
   mainWindow.on("closed", () => {
     mainWindow = null;
-  });
+  });*/
 }
 
 app.on("ready", createWindow);
 app.on("browser-window-created", function(e, window) {
-  window.setMenu(null);
+//  window.setMenu(null);
 });
 
 app.on("window-all-closed", function() {
